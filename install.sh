82edee7176aade7d1c6a7f9eb488bd51cf4c13fa
alexberny <alexberny@yahoo.it>
2021-11-20 20:18:26 +0100

    removed additional  repo

--- install.sh
+++ install.sh
@@ -3,10 +3,6 @@ set -x
 set -e
 PATH=/usr/local/bin:$PATH
 
-sudo dnf config-manager --add-repo https://yum.oracle.com/repo/OracleLinux/OL8/appstream/x86_64/
-sudo dnf config-manager --add-repo https://yum.oracle.com/repo/OracleLinux/OL8/UEKR6/x86_64/
-
-
 # install git
 sudo dnf install git
 
