fb9b56c08f117a716c230a2b6f863033b1e93f5a
alexberny <alexberny@yahoo.it>
2021-11-20 22:21:14 +0100

    ansible playbook first commit

--- install.sh
+++ install.sh
@@ -9,3 +9,14 @@ sudo dnf install git
 # install ansible
 sudo dnf install oracle-epel-release-el8
 sudo dnf install ansible
+
+# get ansible script
+if [ ! -d oracle_ansible ]; then
+    git clone https://github.com/alexberny/oracle_ansible.git
+    cd oracle_ansible
+else
+    cd oracle_ansible
+    git pull
+fi 
+
+ansible-playbook workstation-setup.yml --ask-vault-pass -e@~/password.yaml
